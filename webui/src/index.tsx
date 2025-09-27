317e09507c7065adde1ce4c61c60102c1698e4ca
flameshikari <mail@hexed.pw>
2025-09-27 07:07:32 +0500

    fix index.tsx typo

--- webui/src/index.tsx
+++ webui/src/index.tsx
@@ -13,7 +13,7 @@ const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element nsot found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
   );
 }
 
